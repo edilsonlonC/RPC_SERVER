068fcde32f4ee25ad49f8b1066410391dbdbe30c
Edilson <eddylson.londonio@gmail.com>
2020-03-24 14:16:43 -0500

    try  catch in client for  prompt arguments

--- client/client2.py
+++ client/client2.py
@@ -3,20 +3,24 @@ from xmlrpc.client import Binary
 import sys
 
 argv_list = sys.argv[1::] # terminal arguments
+if not argv_list:
+      raise "send auguments  in prompt"
 argv_list = [int(l) for l in argv_list]
 
 proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
 
-lista = open("lista.bin", "wb+")
-list_sender = argv_list
-sender_b = bytearray(list_sender)
-lista.write(sender_b)
-lista.close()
-with open ("lista.bin","rb") as handle : 
-  
-  lista = proxy.reverse_list(handle.read()).data
-  print (list(lista))
-  
+try: 
+  lista = open("lista.bin", "wb+")
+  list_sender = argv_list
+  sender_b = bytearray(list_sender)
+  lista.write(sender_b)
+  lista.close()
+  with open ("lista.bin","rb") as handle : 
+    
+    lista = proxy.reverse_list(handle.read()).data
+    print (list(lista))
+except:
+  print ("range must be 0 to 256 in list")  
 # with open("lista2.bin", "wb+") as handle:
 #   handle.write(proxy.reverse_list().data)
 #   handle.close()
